52fc1622c423e3973d4ace8e80f003e2b6052ddb
Easbi Ikhsan <easterikhxen@gmail.com>
2020-01-16 16:04:42 +0700

    Weww

--- resources/views/transaksi/show.blade.php
+++ resources/views/transaksi/show.blade.php
@@ -41,12 +41,9 @@
           <td>{{$transaksi->nama_kategori}}</td>
           <td>{{$transaksi->nama_jasa_pengirim}}</td>
           <td>{{ \Carbon\Carbon::parse($transaksi->created_at)->formatLocalized('%d-%b-%Y %H:%M') }} WIB</td>
-          <td>{{$transaksi->nama_status}}</td>          
-<<<<<<< HEAD
+          <td>{{$transaksi->nama_status}}</td>
           <td>{{$transaksi->nama_penerima}}</td>
-=======
           <td>{{$transaksi->nama_petugas}}</td>
->>>>>>> f72e51fd9bf0803e9add60fca08590cf6eb776c9
           <td><a href="{{ action('TransaksiController@edit', $transaksi->id) }}" class="btn btn-warning">Edit</a></td>
         </tr>
         @endforeach
