dc78e4dbad0a4edc664d994b179918a8368b9274
easbi <easterikhxen@gmail.com>
2020-02-20 14:04:39 +0700

    ww

---
+++ resources/views/transaksi/rekapstatus.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.main')
+@section('content')
+ <div class="container">
+      <br />
+      @if (\Session::has('success'))
+      <div class="alert alert-success">
+        <p>{{ \Session::get('success') }}</p>
+      </div><br />
+      @endif
+
+      <h2 align="center">
+        Tabel paket yang 
+        @if($status == 3)
+          sudah diterima penerima
+        @elseif($status == 2)
+          berada di resepsionis
+        @elseif($status == 1)
+          masih  di  pos satpam
+        @endif
+      </h2>
+      <table id="table_id" class="display">
+        <thead>
+          <tr>
+           {{--  <th>ID</th> --}}
+           <th>No</th>
+           <th>Nama Penerima</th>
+           <th>Kode Paket</th>
+           <th>Jenis Diklat</th>
+           <th>Kategori</th>
+           <th>Jasa Pengirim</th>
+           <th>Tanggal Diterima</th>
+           {{-- <th>Tanggal Perubahan Terakhir</th> --}}
+           <th>Status</th>
+           <th>Penerima</th>
+           <th>Edit</th>
+         </tr>
+       </thead>
+       <tbody>
+
+        @foreach($transaksis as $transaksi)
+        <tr>
+          {{-- <td>{{$transaksi['id']}}</td> --}}
+          <td>{{$transaksi->id}}</td> 
+          <td>{{$transaksi->nama_penerima}}</td>
+          <td>{{$transaksi->no_paket}}</td>
+          <td>{{$transaksi->jenis_penerima}}</td>
+          <td>{{$transaksi->nama_kategori}}</td>
+          <td>{{$transaksi->nama_jasa_pengirim}}</td>
+          <td>{{ \Carbon\Carbon::parse($transaksi->created_at)->formatLocalized('%d-%b-%Y %H:%M') }} WIB</td>
+          <td>{{$transaksi->nama_status}}</td>          
+          <td>{{$transaksi->nama_petugas}}</td>
+          <td>
+            <a href="{{ action('TransaksiController@edit', $transaksi->id) }}" class="btn btn-warning">Edit</a>
+            <form action="{{ route('transaksis.destroy', $transaksi->id) }}" method="post">
+              {{ csrf_field() }}
+              {{ method_field('DELETE') }}
+              <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Hapus</button>
+            </form>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <br>
+    <p>Lihat Berdasarkan Waktu</p>
+  </div>
+
+  
+
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+  $(document).ready(function() {
+      $('#table_id').DataTable({
+        "order": [[ 0, "desc" ]],
+        "columnDefs": 
+        [{
+          "targets": 8,
+          "orderable": false
+        }]
+      });
+  });
+  </script>
+@endpush
