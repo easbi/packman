8232a4f0ed32674dc3459058b88a90f2c30be85b
easbi <easterikhxen@gmail.com>
2020-02-20 14:27:13 +0700

    test

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -31,7 +31,7 @@
                 <div id="my-nav" class="collapse navbar-collapse">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item"><a class="nav-link" href="{{ url('/transaksis') }}">Beranda</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/monitoring') }}">Monitoring</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/monitoring') }}">Monitoring (alpha)</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ url('/transaksis/create') }}">Input Paket</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ url('/transaksis/show') }}">Tabel Paket</a></li>
                     </ul>
